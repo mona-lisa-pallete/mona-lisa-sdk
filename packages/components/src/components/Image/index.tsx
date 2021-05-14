2227f0a0da4db7effae3d86d2900038c16b0bf1b
陈炳瑞 <chenbingrui@guorou.tv>
2021-05-14 14:53:12 +0800

    feat: 修改测试 image 组件

--- packages/components/src/components/Image/index.tsx
+++ packages/components/src/components/Image/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const index = () => {
-  return <h1>这是 Image 组件</h1>;
+const index = (props) => {
+  return (
+    <div>
+      <h1>这是 Image 组件</h1>
+      <img style={props.style} src={props.src} />
+    </div>
+  );
 };
 
 export default index;
