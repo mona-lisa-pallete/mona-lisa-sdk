e016db35ecd5677f0ae1f1d560fac1b462a6d18e
陈炳瑞 <chenbingrui@guorou.tv>
2021-05-14 16:42:58 +0800

    feat: 添加测试Taro button

---
+++ packages/components/src/components/Button/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Button } from "@tarojs/components";
+
+const index = () => {
+  return (
+    <View>
+      这是 Taro 的 Button 组件
+      <Button>Button</Button>
+    </View>
+  );
+};
+
+export default index;
