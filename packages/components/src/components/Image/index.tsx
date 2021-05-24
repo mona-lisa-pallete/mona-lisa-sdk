f2505ffa04d414b0dbe96151f39c73fac6543abd
陈炳瑞 <chenbingrui@guorou.tv>
2021-05-24 15:27:05 +0800

    feat: 添加withContext HOC，优化性能

--- packages/components/src/components/Image/index.tsx
+++ packages/components/src/components/Image/index.tsx
@@ -1,9 +1,40 @@
-import React from "react";
-import { Image } from "@tarojs/components";
+import React, { FC, ReactElement, useContext, useMemo } from "react";
+import { Image, View } from "@tarojs/components";
+import { AppContext } from "@davinci/core";
 
+/**
+ * 结合 useMemo，避免因使用 useContext 后的不必要 re-render
+ */
+function withContext(Components: FC, selector: any) {
+  return (props: any): ReactElement => {
+    const { state, dispatch } = useContext(AppContext);
+    const deps = selector(state)(props);
+    return useMemo(
+      () => <Components dispatch={dispatch} {...props} {...deps} />,
+      [...Object.values(deps), ...Object.values(props)]
+    );
+  };
+}
 const index = (props) => {
-  const { contentProp = {}, ...p } = props || {};
-  return <Image {...p} {...contentProp} />;
+  const { id, dispatch, ...p } = props;
+
+  return (
+    <View>
+      <Image
+        {...p}
+        onClick={() => {
+          dispatch({
+            [id]: {
+              time: +new Date(),
+            },
+          });
+        }}
+      />
+    </View>
+  );
 };
 
-export default index;
+export default withContext(index, (state) => (props) => {
+  const { id } = props;
+  return { foo: (state && state[id] && state[id].foo) || "foo" };
+});
