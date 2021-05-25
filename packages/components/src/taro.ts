362f4b1418f5176af32151e0d4bf901ea56c30bb
sanget <zh.sanget@gmail.com>
2021-05-25 19:14:00 +0800

    feat: 完善 taro 的构建脚本,支持只 output h5 的 taro 组件

--- packages/components/src/taro.ts
+++ packages/components/src/taro.ts
@@ -1,5 +1,11 @@
 import * as components from "@tarojs/components/dist-h5/react";
-import * as runtime from "@tarojs/runtime";
-import * as taro from "@tarojs/taro/h5";
+// import * as runtime from "@tarojs/runtime";
+// import * as taro from "@tarojs/taro/h5";
 
-export { components, taro, runtime };
+export { components };
+// export { components, taro, runtime };
+
+export {
+  applyPolyfills,
+  defineCustomElements,
+} from "@tarojs/components/loader";
