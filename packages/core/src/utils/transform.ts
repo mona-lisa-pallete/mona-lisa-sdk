f3722e0e67d47a5599b0117fc80ac36d0d1495ea
陈炳瑞 <chenbingrui@guorou.tv>
2021-06-16 11:18:40 +0800

    feat: pxTransform适配

--- packages/core/src/utils/transform.ts
+++ packages/core/src/utils/transform.ts
@@ -5,5 +5,7 @@ function isNumber(n: any): n is Number {
 }
 
 export function dvPxTransform(size?: string | number) {
-  return isNumber(size) ? pxTransform(size) : size;
+  // 为了适配低版本 pxTransform，添加第二参数
+  // 为了适配B端图片使用，这里乘以2
+  return isNumber(size) ? pxTransform(size * 2, 750) : size;
 }
