dbc200d354bb6abb6ac814372d55a6a1e22d3284
陈炳瑞 <chenbingrui@guorou.tv>
2021-06-16 14:42:34 +0800

    feat: npm 组件

---
+++ packages/dv-video-vertical/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import DvVideoCompBase from "./base"; 
+
+function DvVideoVertical(props) {
+  return (
+    <DvVideoCompBase {...props} type='vertical'/>
+  );
+}
+
+export default DvVideoVertical;
